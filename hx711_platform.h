b6119e6cf1abbfb696f01c79ae697148835ac553
Tamas Benyhe <tamas.benyhe@gmail.com>
2025-05-17 18:16:35 +0200

    Add sisdk-2024.12 compatibility and OTA DFU component

--- hx711_platform.h
+++ hx711_platform.h
@@ -3,10 +3,10 @@
 #include "em_gpio.h"
 #include "sl_emlib_gpio_init_hx711_dt_config.h"
 #include "sl_emlib_gpio_init_hx711_sck_config.h"
-#include "cmsis_compiler.h"
+#include "ustimer.h"
 
 #define clock_high() GPIO_PinOutSet(SL_EMLIB_GPIO_INIT_HX711_SCK_PORT, SL_EMLIB_GPIO_INIT_HX711_SCK_PIN)
 #define clock_low()  GPIO_PinOutClear(SL_EMLIB_GPIO_INIT_HX711_SCK_PORT, SL_EMLIB_GPIO_INIT_HX711_SCK_PIN)
 #define get_DOUT()   GPIO_PinInGet(SL_EMLIB_GPIO_INIT_HX711_DT_PORT, SL_EMLIB_GPIO_INIT_HX711_DT_PIN)
 
-#define delay()      do {__NOP(); __NOP(); __NOP();} while(0)
+#define delay()      USTIMER_DelayIntSafe(1)
